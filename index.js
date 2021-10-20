781bed89c5b4b1fc5691550cc61694b8d517cd5b
zohaib ali shah <zohaibalishah1997@gmail.com>
2021-10-20 15:05:49 +0500

    port

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const express = require('express')
-const { ApolloServer } = require('apollo-server-express')
-const { createServer } = require('http')
-const jwtDecode = require('jwt-decode')
-const typeDefs = require('./src/schema')
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { createServer } = require("http");
+const jwtDecode = require("jwt-decode");
+const typeDefs = require("./src/schema");
 
-const stripeWebhook = require('./src/stripe-webhook')
-const coinbaseWebhook = require('./src/coinbase-webhook')
-const sumsubWebhook = require('./src/sumsub-webhook')
+const stripeWebhook = require("./src/stripe-webhook");
+const coinbaseWebhook = require("./src/coinbase-webhook");
+const sumsubWebhook = require("./src/sumsub-webhook");
 
-const coinbaseWebhookAirstayz = require('./src/coinbase-webhook-airstayz')
-const AirstayzCoinbaseRoute = require('./src/coinbase-airstayz')
+const coinbaseWebhookAirstayz = require("./src/coinbase-webhook-airstayz");
+const AirstayzCoinbaseRoute = require("./src/coinbase-airstayz");
 
-const sendEmail = require('./src/send-email')
-const acceptReferral = require('./src/accept-referral')
-const ServiceImageRoute = require('./src/upload-image')
-const resolvers = require('./src/resolvers')
-const { json, raw } = require('body-parser')
-const createStore = require('./src/lib/createStore')
-const logger = require('./src/lib/logger')
+const sendEmail = require("./src/send-email");
+const acceptReferral = require("./src/accept-referral");
+const ServiceImageRoute = require("./src/upload-image");
+const resolvers = require("./src/resolvers");
+const { json, raw } = require("body-parser");
+const createStore = require("./src/lib/createStore");
+const logger = require("./src/lib/logger");
 
-const authMiddleware = require('./src/lib/utils/authMiddleware.js')
-const authMiddlewareAirstayz = require('./src/lib/utils/authMiddlewareAirstayz.js')
-const authContext = require('./src/lib/utils/authContext')
+const authMiddleware = require("./src/lib/utils/authMiddleware.js");
+const authMiddlewareAirstayz = require("./src/lib/utils/authMiddlewareAirstayz.js");
+const authContext = require("./src/lib/utils/authContext");
 
-const ReferralAPI = require('./src/datasources/referrals')
-const NotificationAPI = require('./src/datasources/notifications')
-const KycAPI = require('./src/datasources/kyc')
-const UserAPI = require('./src/datasources/user')
-const CoinbaseAPI = require('./src/datasources/coinbase')
-const SumSubAPI = require('./src/datasources/sumsub')
-const CheckoutAPI = require('./src/datasources/checkout')
+const ReferralAPI = require("./src/datasources/referrals");
+const NotificationAPI = require("./src/datasources/notifications");
+const KycAPI = require("./src/datasources/kyc");
+const UserAPI = require("./src/datasources/user");
+const CoinbaseAPI = require("./src/datasources/coinbase");
+const SumSubAPI = require("./src/datasources/sumsub");
+const CheckoutAPI = require("./src/datasources/checkout");
 
-const cors = require('cors')
+const cors = require("cors");
 
 const store = createStore();
-const port = 4000
-const app = express()
+const port = 80;
+const app = express();
 
 const dataSources = () => ({
   referralAPI: new ReferralAPI({ store }),
@@ -45,29 +45,29 @@ const dataSources = () => ({
   coinbaseAPI: new CoinbaseAPI({ store }),
   sumsubAPI: new SumSubAPI({ store }),
   checkoutAPI: new CheckoutAPI({ store }),
-})
+});
 
-app.use(cors())
+app.use(cors());
 
 app.use((req, res, next) => {
-  if (req.originalUrl.includes('stripe-webhook')) {
+  if (req.originalUrl.includes("stripe-webhook")) {
     next();
   } else {
     json()(req, res, next);
   }
 });
 
-app.use('/images', authMiddleware, ServiceImageRoute)
-app.use('/coinbase-airstayz', authMiddlewareAirstayz, AirstayzCoinbaseRoute)
-app.post('/stripe-webhook', raw({ type: '*/*' }), stripeWebhook)
-app.post('/coinbase-webhook', coinbaseWebhook)
-app.post('/sumsub-webhook', sumsubWebhook)
+app.use("/images", authMiddleware, ServiceImageRoute);
+app.use("/coinbase-airstayz", authMiddlewareAirstayz, AirstayzCoinbaseRoute);
+app.post("/stripe-webhook", raw({ type: "*/*" }), stripeWebhook);
+app.post("/coinbase-webhook", coinbaseWebhook);
+app.post("/sumsub-webhook", sumsubWebhook);
 
-app.post('/coinbase-webhook-airstayz', coinbaseWebhookAirstayz)
+app.post("/coinbase-webhook-airstayz", coinbaseWebhookAirstayz);
 
-app.post('/accept-referral', acceptReferral)
+app.post("/accept-referral", acceptReferral);
 
-app.post('/email', sendEmail)
+app.post("/email", sendEmail);
 
 const server = new ApolloServer({
   typeDefs,
@@ -75,13 +75,15 @@ const server = new ApolloServer({
   context: authContext,
   dataSources,
   playground: true,
-})
+});
 
-server.applyMiddleware({ app })
-const httpServer = createServer(app)
-server.installSubscriptionHandlers(httpServer)
+server.applyMiddleware({ app });
+const httpServer = createServer(app);
+server.installSubscriptionHandlers(httpServer);
 
 httpServer.listen(port, () => {
-  logger.debug(`server ready at http://localhost:${port}${server.graphqlPath}`)
-  logger.debug(`Subscriptions ready at ws://localhost:${port}${server.subscriptionsPath}`)
-})
+  logger.debug(`server ready at http://localhost:${port}${server.graphqlPath}`);
+  logger.debug(
+    `Subscriptions ready at ws://localhost:${port}${server.subscriptionsPath}`
+  );
+});
